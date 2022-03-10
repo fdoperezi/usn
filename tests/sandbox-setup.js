ea5b7d9d6f9bc5e440370a8906501ce772848aa1
Alexander Fadeev <fadeevab.com@gmail.com>
2022-03-10 19:07:16 +0200

    Add integration tests on sandbox

---
+++ tests/sandbox-setup.js
@@ -0,0 +1,163 @@
+'use strict';
+const nearAPI = require('near-api-js');
+const BN = require('bn.js');
+const fs = require('fs').promises;
+const isReachable = require('is-reachable');
+
+process.env.NEAR_NO_LOGS = 'defined';
+
+const config = {
+  networkId: 'sandbox',
+  nodeUrl: 'http://0.0.0.0:3030',
+  keyPath: '/tmp/near-usn-test-sandbox/validator_key.json',
+  usnPath: './target/wasm32-unknown-unknown/sandbox/usn.wasm',
+  priceoraclePath: './tests/priceoracle.wasm',
+  amount: new BN('10000000000000000000000000', 10), // 25 digits
+  masterId: 'test.near',
+  usnId: 'usn.test.near',
+  oracleId: 'priceoracle.test.near',
+  aliceId: 'alice.test.near',
+  bobId: 'bob.test.near',
+};
+
+const usnMethods = {
+  viewMethods: [
+    'version',
+    'name',
+    'symbol',
+    'decimals',
+    'spread',
+    'contract_status',
+    'get_owner',
+  ],
+  changeMethods: [
+    'new',
+    'upgrade_name_symbol',
+    'upgrade_icon',
+    'blacklist_status',
+    'add_to_blacklist',
+    'remove_from_blacklist',
+    'set_owner',
+    'set_spread',
+    'extend_guardians',
+    'remove_guardians',
+    'destroy_black_funds',
+    'pause',
+    'resume',
+    'buy',
+    'sell',
+    'ft_transfer',
+  ],
+};
+
+const oracleMethods = {
+  changeMethods: ['new', 'add_asset', 'report_prices'],
+};
+
+async function sandboxSetup() {
+  if (!(await isReachable(config.nodeUrl))) {
+    throw new Error('Run sandbox first: `npm run sandbox`!');
+  }
+
+  const keyFile = require(config.keyPath);
+  const privKey = nearAPI.utils.KeyPair.fromString(keyFile.secret_key);
+  const pubKey = nearAPI.utils.PublicKey.fromString(keyFile.public_key);
+
+  const keyStore = new nearAPI.keyStores.InMemoryKeyStore();
+  keyStore.setKey(config.networkId, config.masterId, privKey);
+
+  const near = await nearAPI.connect({
+    deps: {
+      keyStore,
+    },
+    networkId: config.networkId,
+    nodeUrl: config.nodeUrl,
+  });
+
+  // Setup a global test context before anything else failed.
+  global.near = near;
+
+  let masterAccount = new nearAPI.Account(near.connection, config.masterId);
+
+  // Create test accounts.
+  await masterAccount.createAccount(config.usnId, pubKey, config.amount);
+  await masterAccount.createAccount(config.oracleId, pubKey, config.amount);
+  await masterAccount.createAccount(config.aliceId, pubKey, config.amount);
+  await masterAccount.createAccount(config.bobId, pubKey, config.amount);
+  keyStore.setKey(config.networkId, config.usnId, privKey);
+  keyStore.setKey(config.networkId, config.oracleId, privKey);
+  keyStore.setKey(config.networkId, config.aliceId, privKey);
+  keyStore.setKey(config.networkId, config.bobId, privKey);
+
+  // Deploy the USN contract.
+  const wasm = await fs.readFile(config.usnPath);
+  const account = new nearAPI.Account(near.connection, config.usnId);
+  await account.deployContract(wasm);
+
+  // Initialize the contract.
+  const usnContract = new nearAPI.Contract(account, config.usnId, usnMethods);
+  await usnContract.new({ args: { owner_id: config.usnId } });
+
+  // Deploy the priceoracle contract.
+  const wasmPriceoracle = await fs.readFile(config.priceoraclePath);
+  const oracleAccount = new nearAPI.Account(near.connection, config.oracleId);
+  await oracleAccount.deployContract(wasmPriceoracle);
+
+  // Initialize the Oracle contract.
+  const oracleContract = new nearAPI.Contract(
+    oracleAccount,
+    config.oracleId,
+    oracleMethods
+  );
+  await oracleContract.new({ args: { recency_duration_sec: 360 } });
+  await oracleContract.add_asset({ args: { asset_id: 'wrap.test.near' } });
+  await oracleContract.report_prices({
+    args: {
+      prices: [
+        {
+          asset_id: 'wrap.test.near',
+          price: { multiplier: '111439', decimals: 28 },
+        },
+      ],
+    },
+  });
+
+  // Initialize other accounts connected to the contract for all test cases.
+  const alice = new nearAPI.Account(near.connection, config.aliceId);
+  const bob = new nearAPI.Account(near.connection, config.bobId);
+  const aliceContract = new nearAPI.Contract(alice, config.usnId, usnMethods);
+  const bobContract = new nearAPI.Contract(bob, config.usnId, usnMethods);
+
+  // Setup a global test context.
+  global.usnContract = usnContract;
+  global.priceoracleContract = oracleContract;
+  global.aliceContract = aliceContract;
+  global.bobContract = bobContract;
+}
+
+async function sandboxTeardown() {
+  const near = global.near;
+
+  const alice = new nearAPI.Account(near.connection, config.aliceId);
+  const bob = new nearAPI.Account(near.connection, config.bobId);
+  const usn = new nearAPI.Account(near.connection, config.usnId);
+  const oracle = new nearAPI.Account(near.connection, config.oracleId);
+
+  await alice.deleteAccount(config.masterId);
+  await bob.deleteAccount(config.masterId);
+  await usn.deleteAccount(config.masterId);
+  await oracle.deleteAccount(config.masterId);
+}
+
+module.exports = { config, sandboxSetup, sandboxTeardown };
+
+module.exports.mochaHooks = {
+  beforeAll: async function () {
+    this.timeout(30000);
+    await sandboxSetup();
+  },
+  afterAll: async function () {
+    this.timeout(10000);
+    await sandboxTeardown();
+  },
+};
---
+++ tests/tests.js
@@ -0,0 +1,121 @@
+'use strict';
+
+const { globalAgent } = require('http');
+
+const assert = require('assert').strict;
+const config = require('./sandbox-setup').config;
+
+describe('Anyone', function () {
+  it('should get a version (smoke test)', async () => {
+    const version = await global.aliceContract.version();
+    assert.match(version, /0\.1\./);
+  });
+});
+
+describe('Anyone', function () {
+  it('should get a name', async () => {
+    const name = await global.aliceContract.name();
+    assert.equal(name, 'USN');
+  });
+
+  it('should get a symbol', async () => {
+    const symbol = await global.aliceContract.symbol();
+    assert.equal(symbol, 'USN');
+  });
+
+  it('should get decimals', async () => {
+    const decimals = await global.aliceContract.decimals();
+    assert.equal(decimals, 18);
+  });
+
+  it('should get a spread', async () => {
+    const spread = await global.aliceContract.spread();
+    assert.equal(spread, 10000);
+  });
+
+  it('should get contract status', async () => {
+    const status = await global.aliceContract.contract_status();
+    assert.equal(status, 'Working');
+  });
+
+  it('should get an owner', async () => {
+    const owner = await global.aliceContract.get_owner();
+    assert.equal(owner, config.usnId);
+  });
+});
+
+describe('Owner', function () {
+  this.timeout(5000);
+
+  it('should be able to assign guardians', async () => {
+    await assert.doesNotReject(async () => {
+      await global.usnContract.extend_guardians({
+        args: { guardians: [config.aliceId] },
+      });
+    });
+  });
+
+  it('should be able to remove guardians', async () => {
+    await assert.doesNotReject(async () => {
+      await global.usnContract.extend_guardians({
+        args: { guardians: [config.aliceId] },
+      });
+      await global.usnContract.remove_guardians({
+        args: { guardians: [config.aliceId] },
+      });
+    });
+  });
+});
+
+describe('Owner', function () {
+  this.timeout(5000);
+
+  before(async () => {
+    await global.usnContract.set_owner({
+      args: { owner_id: config.aliceId },
+    });
+    assert.equal(await global.usnContract.get_owner(), config.aliceId);
+  });
+
+  it('can lost ownership', async () => {
+    assert.rejects(async () => {
+      await global.usnContract.set_owner({ args: { owner_id: config.usnId } });
+    });
+  });
+
+  after(async () => {
+    await global.aliceContract.set_owner({
+      args: { owner_id: config.usnId },
+    });
+    assert.equal(await global.aliceContract.get_owner(), config.usnId);
+  });
+});
+
+describe('Guardian', function () {
+  this.timeout(5000);
+
+  before(async () => {
+    await global.usnContract.extend_guardians({
+      args: { guardians: [config.aliceId] },
+    });
+  });
+
+  it('should be able to pause the contract', async () => {
+    assert.doesNotReject(async () => {
+      await global.aliceContract.pause({ args: {} });
+      assert.equal(await global.aliceContract.contract_status(), 'Paused');
+    });
+
+    assert.rejects(async () => {
+      await global.aliceContract.ft_transfer({
+        args: { receiver_id: 'any', amount: '1' },
+      });
+    });
+  });
+
+  after(async () => {
+    await global.usnContract.remove_guardians({
+      args: { guardians: [config.aliceId] },
+    });
+  });
+});
