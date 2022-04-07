b1f0d68d35fb1e50e69b82b4db493542768102c4
dariacherniakk <dc@binary-star.org>
2022-04-07 18:30:16 +0300

    store commission

--- tests/tests.js
+++ tests/tests.js
@@ -56,16 +56,6 @@ describe('Anyone', function () {
       available: '0',
     });
   });
-
-  it('should get a usn commission', async () => {
-    const commission_usn = await global.aliceContract.commission_usn();
-    assert.equal(commission_usn, '98942062800000000');
-  });
-
-  it('should get a near commission', async () => {
-    const commission_near = await global.aliceContract.commission_usn();
-    assert.equal(commission_near, '10498000000000000000000');
-  });
 });
 
 describe('Owner', function () {
@@ -88,6 +78,16 @@ describe('Owner', function () {
         args: { guardians: [config.aliceId] },
       });
     });
+  });
+
+  it('should get a usn commission', async () => {
+    const commission_usn = await global.aliceContract.commission_usn();
+    assert.equal(commission_usn, '98942062800000000');
+  });
+
+  it('should get a near commission', async () => {
+    const commission_near = await global.aliceContract.commission_near();
+    assert.equal(commission_near, '10498000000000000000000');
   });
 });
 
